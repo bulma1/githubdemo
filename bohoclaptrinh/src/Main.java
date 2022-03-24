9a8b42bba636a08a393c16951dd63cc8b74e7c10
naul2354 <lyruz2354@gmail.com>
2022-03-24 10:39:31 +0700

    Thêm 1 file java

---
+++ bohoclaptrinh/src/Main.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Main {
+    static void checkAge(int age ) {
+        if (age < 18) {
+            System.out.println("Quá non");
+        }else{
+            System.out.println(" So younggggggg");
+        }
+    }
+    public static void main(String[] args) {
+        checkAge(20);
+
+
+    }
+}
+
