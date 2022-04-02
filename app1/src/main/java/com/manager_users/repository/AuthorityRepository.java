82c426d9ad38896a0acb9f9d6df6d32d50bf7a84
naul2354 <lyruz2354@gmail.com>
2022-04-02 15:51:41 +0700

    Initial version of manager_users generated by generator-jhipster@7.6.0

---
+++ app1/src/main/java/com/manager_users/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.manager_users.repository;
+
+import com.manager_users.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
