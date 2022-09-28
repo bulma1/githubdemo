c519a4bfd1b0bc5cf9f3fa37fe8349b43ad2933c
naul2354 <lyruz2354@gmail.com>
2022-09-28 08:10:38 +0700

    Initial version of sylas generated by generator-jhipster@7.6.0

---
+++ Desktop/jhipster1/src/main/java/sylas/com/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * JPA domain objects.
+ */
+package sylas.com.domain;
---
+++ Desktop/jhipster1/src/test/java/sylas/com/IntegrationTest.java
@@ -0,0 +1,17 @@
+package sylas.com;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import sylas.com.SylasApp;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = SylasApp.class)
+public @interface IntegrationTest {
+}
